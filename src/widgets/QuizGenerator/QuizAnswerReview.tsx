187be0e17bd0b593bee2a10620ad6a0e75292278
HisokaWizard <npa040493@gmail.com>
2025-07-24 23:24:27 +0300

    Change localization for review component

--- src/widgets/QuizGenerator/QuizAnswerReview.tsx
+++ src/widgets/QuizGenerator/QuizAnswerReview.tsx
@@ -13,15 +13,15 @@ type QuizAnswerReviewProps = {
 export const QuizAnswerReview = ({ answers }: QuizAnswerReviewProps) => {
   return (
     <div className="answer-review">
-      <h3 className="font-fantasy">Обзор</h3>
+      <h3 className="font-fantasy">Review</h3>
       {answers.map((answer, index) => (
         <div key={index} className="answer-item">
           <p className="question">{answer.question}</p>
-          <p className="correct-answer">Правильный: {answer.correctAnswer}</p>
+          <p className="correct-answer">Correct: {answer.correctAnswer}</p>
           <p
             className={`player-answer ${answer.playerAnswer === answer.correctAnswer ? 'correct' : 'incorrect'}`}
           >
-            {answer.playerAnswer === answer.correctAnswer ? '✅' : '❌'} Ваш:{' '}
+            {answer.playerAnswer === answer.correctAnswer ? '✅' : '❌'} Your:{' '}
             {answer.playerAnswer}
           </p>
         </div>
